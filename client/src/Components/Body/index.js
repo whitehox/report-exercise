f4a24d3c3d9be7d852501e9662f2d75f4778ff40
Tega Oke <oketegah@gmail.com>
2019-06-26 17:09:04 +0100

    Get driver function gets drivers

--- client/src/Components/Body/index.js
+++ client/src/Components/Body/index.js
@@ -3,16 +3,20 @@ import './Body.css';
 
 /** Helper Functions */
 import getTrips from '../../Helpers/getTrips';
+import getDrivers from '../../Helpers/getDrivers';
 
 /**Components */
 import SearchBar from '../SearchBar/';
 
 function Body(props) {
   const trips = getTrips();
+  const drivers = getDrivers();
   return (
     <main className="mainBody">
       <SearchBar />
-      <div>{trips}</div>
+      <div>
+        {trips} {drivers}
+      </div>
     </main>
   );
 }
