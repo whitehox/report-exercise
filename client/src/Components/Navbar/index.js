5a8e830e9743e86359f932befd9a95b6fdb42481
Tega Oke <oketegah@gmail.com>
2019-06-27 14:32:56 +0100

    Added navbar links

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './Navbar.css';
 
 function Navbar() {
-  return <nav className="mainNav" />;
+  return (
+    <nav className="mainNav">
+      <p>MAIN NAVIGATION</p>
+      <ul>
+        <a href="/#">
+          <li>
+            <i className="mdi mdi-view-dashboard" /> Dashboard
+          </li>
+        </a>
+        <a href="/#">
+          <li>
+            <i className="mdi mdi-map-check" /> Trips
+          </li>
+        </a>
+        <a href="/#">
+          <li>
+            <i className="mdi mdi-account-card-details" /> Drivers
+          </li>
+        </a>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
